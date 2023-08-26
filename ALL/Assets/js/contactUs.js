5979b8d25aca533ef096eece5284bc5871a4903e
kayden <nguyendaithinh3004@gmail.com>
2023-08-26 09:53:32 +0700

    push cai j do

--- ALL/Assets/js/contactUs.js
+++ ALL/Assets/js/contactUs.js
@@ -92,3 +92,22 @@ var contactUs__nav_listContainer2 = document.querySelector('.contactUs__nav-list
 contactUs__nav_listContainer_Menubtn.addEventListener('click', function() {
     contactUs__nav_listContainer2.classList.toggle("openMenu")
 })
+
+
+// =================Toan's Nav========================
+let navbar = document.querySelector(".navbar");
+
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function() {
+navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function() {
+navLinks.style.left = "-100%";
+}
+
+let productsArrow = document.querySelector(".products-arrow");
+productsArrow.onclick = function() {
+ navLinks.classList.toggle("show1");
+}
